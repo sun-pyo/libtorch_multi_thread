8dee513f817066f7cd49f6f379bf83e5e1fb8101
sun-pyo <sunpyu97@gmail.com>
2021-02-22 16:24:22 +0900

    vgg 실행

--- include/vgg.h
+++ include/vgg.h
@@ -5,7 +5,7 @@
 #include "test.h"
 #include "thpool.h"
 
-void get_submodule_vgg(torch::jit::script::Module module, std::vector<torch::jit::Module> &child);
+void get_submodule_vgg(torch::jit::script::Module module,Net &net);
 void *predict_vgg(Net *input);
 
 void forward_vgg(th_arg *th);
